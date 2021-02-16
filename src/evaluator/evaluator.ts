7c1ce00d2ca5e1f659b0e9727e612afeaab49741
Noel Kwan <noelkwan1998@gmail.com>
2021-02-17 00:56:29 +0800

    Evaluate normal form primitives

--- src/evaluator/evaluator.ts
+++ src/evaluator/evaluator.ts
@@ -1,7 +1,16 @@
-import { Result, err } from '../utils';
+import { Result, err, ok } from '../utils';
 import { SExpr } from '../sexpr';
 
 export function evaluate(program: SExpr): Result<SExpr, void> {
-  program;
+  // Normal form
+  if (program._type === 'SNumber') {
+    return ok(program);
+  }
+  if (program._type === 'SBoolean') {
+    return ok(program);
+  }
+  if (program._type === 'SNil') {
+    return ok(program);
+  }
   return err();
 }
