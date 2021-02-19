1d4354a9000750ff5499f284fe0d93d08c39d7d8
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2021-02-19 18:51:36 +0800

    Add some example code as tests

---
+++ src/__tests__/examples.ts
@@ -0,0 +1,57 @@
+import { print } from '../printer';
+import { read } from '../reader';
+import { evaluate, the_global_environment } from '../evaluator';
+import { getOk } from '../utils';
+
+function expectReadEvalPrint(program: string) {
+  return expect(print(getOk(evaluate(getOk(read(program)), the_global_environment))));
+}
+
+test('plus one', () => {
+  expectReadEvalPrint('(let ([plus1 (lambda (x) (+ x 1))]) (plus1 1))').toMatchInlineSnapshot(
+    `"2"`
+  );
+});
+
+test('thrice thrice', () => {
+  expectReadEvalPrint(`
+    (begin
+      (define (thrice f)
+        (lambda (x) (f (f (f x)))))
+      (((thrice thrice) add1) 0))
+    `).toMatchInlineSnapshot(`"27"`);
+});
+
+test('fibonacci', () => {
+  expectReadEvalPrint(`
+    (begin
+      (define (fib n)
+         (cond [(<= n 1) n]
+               [true (+ (fib (- n 1))
+                        (fib (- n 2)))]))
+      (fib 7))
+    `).toMatchInlineSnapshot(`"13"`);
+});
+
+test("newton's method sqrt", () => {
+  expectReadEvalPrint(`
+    (begin
+      (define tolerance 0.000000000000001)
+      (define (close-enough x y)
+        (< (abs (- x y)) tolerance))
+      (define (fixed-point f first-guess)
+        (define (try-with guess)
+          (define next (f guess))
+          (cond [(close-enough guess next) next]
+                [#t (try-with next)]))
+        (try-with first-guess))
+      (define (average x y) (/ (+ x y) 2))
+      (define (sqrt x)
+        (fixed-point
+          (lambda (y)
+            (average y (/ x y)))
+          1))
+      (list (sqrt 5)                ; square root of 5
+            (* (sqrt 5) (sqrt 5)))) ; square of square root of 5
+    `).toMatchInlineSnapshot(`"(2.23606797749979 5.000000000000001)"`);
+});
