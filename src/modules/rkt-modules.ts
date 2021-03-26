5c459feb6698fc7f12747c377648975485dcd505
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2021-03-26 13:56:38 +0800

    Implement a queue data structure in racket

--- src/modules/rkt-modules.ts
+++ src/modules/rkt-modules.ts
@@ -6,14 +6,16 @@ import cond_lib from './rkt/cond.rkt';
 import define_provide_lib from './rkt/define+provide.rkt';
 import let_lib from './rkt/let.rkt';
 import quasiquote_lib from './rkt/quasiquote.rkt';
+import queue_lib from './rkt/queue.rkt';
 
 export const libs: readonly [string, string][] = [
   ['/libs/racket/private/and-or.rkt', and_or_lib],
   ['/libs/racket/base.rkt', base_lib],
   ['/libs/racket/private/car-et-al.rkt', car_et_al_lib],
   ['/libs/racket/private/compose.rkt', compose_lib],
+  ['/libs/racket/private/cond.rkt', cond_lib],
   ['/libs/racket/private/define+provide.rkt', define_provide_lib],
   ['/libs/racket/private/let.rkt', let_lib],
   ['/libs/racket/private/quasiquote.rkt', quasiquote_lib],
-  ['/libs/racket/private/cond.rkt', cond_lib],
+  ['/libs/racket/private/queue.rkt', queue_lib],
 ];
