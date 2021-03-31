dee687c5d4f9a4b7a6e489ecaac7f1e80ccd4f98
Noel Kwan <noelkwan1998@gmail.com>
2021-03-31 21:47:56 +0800

    Add verbose flag

--- src/repl/repl.ts
+++ src/repl/repl.ts
@@ -2,9 +2,20 @@ import { compile_and_run_test } from '../testing/test-runner';
 import { readFile } from 'fs';
 import { resolve } from 'path';
 
+type Args = {
+  isVerbose: boolean; // Verbose prints compilation result
+  filePath: string;
+};
+
 export function startRepl() {
-  const fileName = resolve(process.argv[2]);
-  readFile(fileName, 'utf8', (err, data) => {
+  const result = parseArgs(process.argv);
+  if (result === undefined) {
+    return;
+  }
+
+  const { isVerbose, filePath } = result;
+
+  readFile(filePath, 'utf8', (err, data) => {
     // Readfile error
     if (err) {
       console.error(err);
@@ -22,15 +33,17 @@ export function startRepl() {
       return;
     }
 
-    const compiled = result['compiled'];
-    if (compiled && compiled['good']) {
-      console.log('COMPILATION RESULT:');
-      console.log(compiled['v']);
-      console.log('');
-    } else {
-      console.error('COMPILATION ERROR:');
-      console.error(compiled);
-      return;
+    if (isVerbose) {
+      const compiled = result['compiled'];
+      if (compiled && compiled['good']) {
+        console.log('COMPILATION RESULT:');
+        console.log(compiled['v']);
+        console.log('');
+      } else {
+        console.error('COMPILATION ERROR:');
+        console.error(compiled);
+        return;
+      }
     }
 
     const evaluated = result['evaluated'];
@@ -43,4 +56,21 @@ export function startRepl() {
       console.error(evaluated);
     }
   });
+}
+
+function parseArgs(args: string[]): Args | undefined {
+  // arg[0] == yarn
+  // arg[1] == repl
+  // arg[2:] actual arguments
+
+  try {
+    const arg1 = args[2];
+    if (arg1 === '-v' || arg1 === '--verbose') {
+      return { isVerbose: true, filePath: resolve(args[3]) };
+    }
+    return { isVerbose: false, filePath: resolve(process.argv[2]) };
+  } catch (_e) {
+    console.log('Usage: yarn repl [-v | --verbose] FILEPATH');
+    return undefined;
+  }
 }
