6b7e82a26b85386ae778e07ef6940fcd745b0a57
Christian James Welly <chrisjwelly@gmail.com>
2021-04-03 15:23:35 +0800

    Add specification

--- src/fep-types.ts
+++ src/fep-types.ts
@@ -43,17 +43,72 @@ export type DefineForm = SCons<Token<'define'>, SCons<SSymbol, SCons<ExprForm, S
 export type DefineSyntaxForm = SCons<Token<'define-syntax'>, SCons<SSymbol, SCons<ExprForm, SNil>>>;
 export type RequireForm = SCons<Token<'#%require'>, SHomList<SExpr>>;
 
+// program:
+//  - function table,
+//      where the 1st function (function 0) is the one executed when the program starts)
+//      func id -> func body (an instruction array)
+//  - name translation table:
+//      name id -> name (string)
+//      name -> name id (at compile time, we need this)
+//  - constant table:
+//      const id -> value (sexpr)
+//  - require table
+//      list of require specs (see other stuff for how to read this)
+//  - provide table
+//      list of provide specs (see other stuff for how to read this)
+
+// runtime state:
+//  - OS (operand stack)
+//  - ENV (environment, see src/environment/environment.ts)
+//  - FUNC ID (see function table above)
+//  - PC (program counter, index inside the body of the currently executing function)
+//  - RTS (runtime stack, used when we reach the end of a function body, we pop the RTS)
+//    - stack of (OS, ENV, FUNC ID, PC)
+
+// type DefineForm = SCons<Token<'define'>, SCons<SSymbol, SCons<ExprForm, SNil>>>;
+// <expr>,
+// add_binding <name>
+// type DefineSyntaxForm = SCons<Token<'define-syntax'>, SCons<SSymbol, SCons<ExprForm, SNil>>>;
+// <expr>,
+// add_syntax_binding <name>
+
 export type ExprForm =
-  | PlainLambdaForm
   | IfForm
-  | BeginForm
-  | Begin0Form
+  // <cond_expr>,
+  // jump_if_false (absolute position of alt_expr),
+  // <then_expr>,
+  // jump (absolute position after alt_expr),
+  // <alt_expr>
+  | BeginForm // <expr1>,<expr2>,...popN <n-1>,<exprn>
+  | Begin0Form // <expr1>,<expr2>,...,<exprn>,popN <n-1>
+  | PlainLambdaForm // expression: make_func <func id> // get_func
+  // when compiling this, add new function to function table, <body>
   | LetForm
+  // <expr1>,
+  // ...,
+  // <exprn>,
+  // extend_env,
+  // add_binding <namen id>,
+  // add_binding <name(n-1) id>,
+  // ...,
+  // add_binding <name1 id>
   | LetrecForm
-  | QuoteForm
-  | PlainAppForm
-  | VariableReferenceForm
-  | SetForm;
+  // extend_env,
+  // add_binding_undefined <name1 id>
+  // add_binding_undefined <name2 id>,
+  // ...,
+  // add_binding_undefined <namen id>
+  // <expr1>,
+  // set_env <name1 id>,
+  // <expr2>,
+  // set_env <name2 id>,
+  // ...,
+  // <exprn>,
+  // set_env <namen id>,
+  | QuoteForm // make_const <const id>                          // get_const
+  | PlainAppForm // <func_expr>,<arg1_expr>,...<argn_expr>,call n // extend_env, push_rts
+  | VariableReferenceForm // get_env <name id>                  // get_env
+  | SetForm; // set_env <name id>                               // set_env
 
 export type PlainLambdaForm = SCons<
   Token<'#%plain-lambda'>,
