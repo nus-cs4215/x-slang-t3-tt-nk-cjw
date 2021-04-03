2586fc55859ec9a7eba19938db1ca35be2a7db80
Christian James Welly <chrisjwelly@gmail.com>
2021-04-03 21:23:02 +0800

    Add untested compilation of let to bytecode

--- src/virtual_machine/compiler.ts
+++ src/virtual_machine/compiler.ts
@@ -1,10 +1,10 @@
-import { Begin0Form, BeginForm, ExprForm, ExprOrDefineAst, QuoteForm } from '../fep-types';
+import { Begin0Form, BeginForm, ExprForm, ExprOrDefineAst, LetForm, QuoteForm } from '../fep-types';
 import { car, cdr, is_list, SExpr, SHomList, val } from '../sexpr';
 import { homlist_to_arr } from '../sexpr/sexpr';
 import { flatten_compiled_program_tree } from './utils';
 
 export interface ProgramState {
-  nameToNameId: Map<number, string>;
+  nameToNameId: Map<string, number>;
   nameIdToName: string[];
   constIdToSExpr: SExpr[];
 }
@@ -14,11 +14,13 @@ export type CompiledProgramTree = number | CompiledProgramTree[];
 // OpCodes
 const MAKE_CONST = 0; // followed by a <const id>
 const POP = 1; // followed by n, number of things to pop
+const ADD_BINDING = 2; // followed by a <name id>
 
 const get_opcode_names = (): string[] => {
   const names = [];
   names[MAKE_CONST] = 'MAKE_CONST';
   names[POP] = 'POP';
+  names[ADD_BINDING] = 'ADD_BINDING';
   return names;
 };
 
@@ -26,6 +28,7 @@ const get_opcode_paramCounts = (): number[] => {
   const paramCounts = [];
   paramCounts[MAKE_CONST] = 1;
   paramCounts[POP] = 1;
+  paramCounts[ADD_BINDING] = 1;
   return paramCounts;
 };
 
@@ -93,6 +96,50 @@ const fep_to_bytecode_helper = (
 
       compiledProgramTree.push(fep_to_bytecode_helper(sequence[sequence.length - 1], programState));
 
+      return compiledProgramTree;
+    }
+    case 'let': {
+      const letprogram = program as LetForm;
+
+      const binding_pairs = homlist_to_arr(car(cdr(letprogram)));
+      // adding the exprs into the stack first
+      for (let i = 0; i < binding_pairs.length; i++) {
+        const binding_pair = binding_pairs[i];
+        const symbol = car(binding_pair);
+        const expr = car(cdr(binding_pair));
+
+        const nameId = programState.nameToNameId.get(val(symbol));
+        if (nameId === undefined) {
+          programState.nameToNameId.set(val(symbol), programState.nameIdToName.length);
+          programState.nameIdToName.push(val(symbol));
+        }
+        compiledProgramTree.push(fep_to_bytecode_helper(expr, programState));
+      }
+
+      // adding instructions to bind the names on top of stack in reverse order
+      for (let i = binding_pairs.length - 1; i >= 0; i--) {
+        const binding_pair = binding_pairs[i];
+        const symbol = car(binding_pair);
+
+        const nameId = programState.nameToNameId.get(val(symbol));
+        if (nameId === undefined) {
+          console.error('nameId is undefined when it should not have been');
+          return compiledProgramTree;
+        }
+        compiledProgramTree.push(ADD_BINDING);
+        compiledProgramTree.push(nameId);
+      }
+
+      // evaluates the expr in the let body and return the last one
+      const sequence: ExprForm[] = homlist_to_arr(cdr(cdr(letprogram)));
+      for (let i = 0; i < sequence.length - 1; i++) {
+        const expr = sequence[i];
+        compiledProgramTree.push(fep_to_bytecode_helper(expr, programState));
+      }
+      compiledProgramTree.push(POP);
+      compiledProgramTree.push(sequence.length - 1);
+
+      compiledProgramTree.push(fep_to_bytecode_helper(sequence[sequence.length - 1], programState));
       return compiledProgramTree;
     }
     default: {
