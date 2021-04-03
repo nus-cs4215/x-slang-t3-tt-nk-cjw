810af35f8619e83bd35c0fe045781adb8a2f3ccb
Christian James Welly <chrisjwelly@gmail.com>
2021-04-04 00:16:18 +0800

    Add test to compile let to bytecode

--- src/virtual_machine/__tests__/compiler.ts
+++ src/virtual_machine/__tests__/compiler.ts
@@ -206,3 +206,166 @@ test('compile #%variable-reference', () => {
     }
   `);
 });
+
+test('compile let', () => {
+  const programState1 = make_program_state();
+  expect(
+    compileAndPrettify(
+      getOk(read(`(let ([x (quote 1)]) (#%variable-reference x))`)) as ExprOrDefineForm,
+      programState1
+    )
+  ).toMatchInlineSnapshot(`
+    Array [
+      "MAKE_CONST",
+      "0",
+      "ADD_BINDING",
+      "0",
+      "POP_N",
+      "0",
+      "GET_ENV",
+      "0",
+    ]
+  `);
+
+  const programState2 = make_program_state();
+  expect(
+    compileAndPrettify(
+      getOk(
+        read(`(let ([x (#%variable-reference y)]) (#%variable-reference x))`)
+      ) as ExprOrDefineForm,
+      programState2
+    )
+  ).toMatchInlineSnapshot(`
+    Array [
+      "GET_ENV",
+      "0",
+      "ADD_BINDING",
+      "1",
+      "POP_N",
+      "0",
+      "GET_ENV",
+      "1",
+    ]
+  `);
+
+  const programState3 = make_program_state();
+  expect(
+    compileAndPrettify(
+      getOk(
+        read(
+          `(let ([x (quote 2)] [y (quote 3)] [z (quote 4)] [a (quote 5)]) (#%variable-reference y))`
+        )
+      ) as ExprOrDefineForm,
+      programState3
+    )
+  ).toMatchInlineSnapshot(`
+    Array [
+      "MAKE_CONST",
+      "0",
+      "MAKE_CONST",
+      "1",
+      "MAKE_CONST",
+      "2",
+      "MAKE_CONST",
+      "3",
+      "ADD_BINDING",
+      "0",
+      "ADD_BINDING",
+      "1",
+      "ADD_BINDING",
+      "2",
+      "ADD_BINDING",
+      "3",
+      "POP_N",
+      "0",
+      "GET_ENV",
+      "2",
+    ]
+  `);
+  // start assigning the names in reverse order
+  expect(programState3.nameToNameId).toMatchInlineSnapshot(`
+    Map {
+      "a" => 0,
+      "z" => 1,
+      "y" => 2,
+      "x" => 3,
+    }
+  `);
+
+  const programState4 = make_program_state();
+  expect(
+    compileAndPrettify(
+      getOk(
+        read(`(let ([x (quote 2)] [y (#%variable-reference x)]) (#%variable-reference y))`)
+      ) as ExprOrDefineForm,
+      programState4
+    )
+  ).toMatchInlineSnapshot(`
+    Array [
+      "MAKE_CONST",
+      "0",
+      "GET_ENV",
+      "0",
+      "ADD_BINDING",
+      "1",
+      "ADD_BINDING",
+      "0",
+      "POP_N",
+      "0",
+      "GET_ENV",
+      "1",
+    ]
+  `);
+  expect(programState4.nameToNameId).toMatchInlineSnapshot(`
+    Map {
+      "x" => 0,
+      "y" => 1,
+    }
+  `);
+
+  const programState5 = make_program_state();
+  expect(
+    compileAndPrettify(
+      getOk(
+        read(
+          `(let ([x (quote 2)] [y (quote 3)] [z (quote 4)] [a (quote 5)]) (#%variable-reference y) (#%variable-reference a))`
+        )
+      ) as ExprOrDefineForm,
+      programState5
+    )
+  ).toMatchInlineSnapshot(`
+    Array [
+      "MAKE_CONST",
+      "0",
+      "MAKE_CONST",
+      "1",
+      "MAKE_CONST",
+      "2",
+      "MAKE_CONST",
+      "3",
+      "ADD_BINDING",
+      "0",
+      "ADD_BINDING",
+      "1",
+      "ADD_BINDING",
+      "2",
+      "ADD_BINDING",
+      "3",
+      "GET_ENV",
+      "2",
+      "POP_N",
+      "1",
+      "GET_ENV",
+      "0",
+    ]
+  `);
+  // starts assigning the name in reverse order
+  expect(programState5.nameToNameId).toMatchInlineSnapshot(`
+    Map {
+      "a" => 0,
+      "z" => 1,
+      "y" => 2,
+      "x" => 3,
+    }
+  `);
+});
