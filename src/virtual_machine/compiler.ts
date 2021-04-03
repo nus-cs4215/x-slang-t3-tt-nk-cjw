ad2592dbabd68643acfb33bee2abe0c48653d6ed
Christian James Welly <chrisjwelly@gmail.com>
2021-04-03 18:21:08 +0800

    Implement quote compiler and flatten tree

--- src/virtual_machine/compiler.ts
+++ src/virtual_machine/compiler.ts
@@ -1,5 +1,5 @@
-import { ExprOrDefineAst } from '../fep-types';
-import { SExpr } from '../sexpr';
+import { ExprOrDefineAst, QuoteForm } from '../fep-types';
+import { car, cdr, SExpr, val } from '../sexpr';
 
 export interface ProgramState {
   nameToNameId: Map<number, string>;
@@ -8,6 +8,9 @@ export interface ProgramState {
 }
 
 export type CompiledProgramTree = number | CompiledProgramTree[];
+
+// OpCodes
+const MAKE_CONST = 0; // followed by a <const id>
 
 export const make_program_state = (): ProgramState => ({
   nameToNameId: new Map(),
@@ -18,17 +21,45 @@ export const make_program_state = (): ProgramState => ({
 export const compile_fep_to_bytecode = (
   program: ExprOrDefineAst,
   programState: ProgramState
-): CompiledProgramTree => {
+): CompiledProgramTree[] => {
   return fep_to_bytecode_helper(program, programState, []);
 };
 
 const fep_to_bytecode_helper = (
   program: ExprOrDefineAst,
   programState: ProgramState,
-  compiledProgramTree: CompiledProgramTree
-): CompiledProgramTree => {
-  console.log(programState);
-  console.log(program);
+  compiledProgramTree: CompiledProgramTree[]
+): CompiledProgramTree[] => {
+  const token_val = val(car(program));
+  switch (token_val) {
+    case 'quote': {
+      const quoteprogram = program as QuoteForm;
 
-  return compiledProgramTree;
+      const quoteProgramTree: CompiledProgramTree[] = [];
+      quoteProgramTree.push(MAKE_CONST);
+      quoteProgramTree.push(programState.constIdToSExpr.length);
+
+      const sexpr = car(cdr(quoteprogram));
+      programState.constIdToSExpr.push(sexpr);
+      return compiledProgramTree;
+    }
+    default: {
+      console.log('Inside default');
+      return compiledProgramTree;
+    }
+  }
+};
+
+export const flatten_compiled_program_tree = (
+  tree: CompiledProgramTree,
+  flat: CompiledProgramTree[] = []
+): CompiledProgramTree[] => {
+  if (Array.isArray(tree)) {
+    for (const child of tree) {
+      flatten_compiled_program_tree(child, flat);
+    }
+  } else {
+    flat.push(tree);
+  }
+  return flat;
 };
