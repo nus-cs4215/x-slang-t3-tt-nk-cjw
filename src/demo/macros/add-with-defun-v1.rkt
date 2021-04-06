475c5e30cea493ca31bbde9237a76eb8ff20973e
Noel Kwan <noelkwan1998@gmail.com>
2021-04-06 16:07:36 +0800

    Cleanup output from macro demo scripts

--- src/demo/macros/add-with-defun-v1.rkt
+++ src/demo/macros/add-with-defun-v1.rkt
@@ -1,5 +1,5 @@
 ; Defun which supports a single body statement
-(define-syntax defun1
+(define-syntax defun
    (#%plain-lambda (stx)
                    (let ([func-name (car (cdr stx))]
                          [args (car (cdr (cdr stx)))]
@@ -10,7 +10,7 @@
                      (list 'define func-name
                            (list '#%plain-lambda args body)))))
 
-(defun1 add_ (a b)
+(defun add (a b)
   (+ a b)) ; Only 1 body statement
 
-(add_ 1 2)
+(add 1 2)
